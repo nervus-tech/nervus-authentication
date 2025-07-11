6514d4ac1f92e417f0effc25865125de3c7478f8
nervus-tech <nervus.tech@gmail.com>
2025-07-11 12:58:12 +0300

    Add test endpoint to AuthController for service verification

--- authentication/src/main/java/com/nervus/authentication/controller/AuthController.java
+++ authentication/src/main/java/com/nervus/authentication/controller/AuthController.java
@@ -1,5 +1,12 @@
 package com.nervus.authentication.controller;
 
-public class AuthController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class AuthController {
+    @GetMapping("/test")
+    public String test() {
+        return "Authentication Service is running";
+    }
 }
